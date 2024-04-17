9adf4db1019375ee8e5614e2b9d67ffaef73f5a3
Ensar Emir Erol <ensaremir.erol99@gmail.com>
2024-04-17 15:15:45 +0200

    merge

--- bootstrap.py
+++ bootstrap.py
@@ -2,14 +2,14 @@
 This file is used to bootstrap the application. It is called from the main
 """
 
-from kink import di
-from pathlib import Path
-from dotenv import load_dotenv
 import os
+from pathlib import Path
+
+from dotenv import load_dotenv
+from kink import di
 
 
-def bootstrap():
+async def bootstrap():
     load_dotenv()
     # Either assign the env variables to global variables or use some kind of DI
     # di["temp_dir"] = Path(os.getenv("TEMP_DIR") or "/app_tmp")
-    
